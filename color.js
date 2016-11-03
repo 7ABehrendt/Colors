d3805f1d990f13c0021e3597e292c1ae2c871a3b
7ABehrendt <7abehrendt@stpatrick.org>
2016-11-03 11:26:27 -0500

    finished the intensities

--- color.js
+++ color.js
@@ -1,5 +1,5 @@
 //Declare the Color Object with our new keyword below here.
-const Color = new object
+
 
 /*
 ** rgb is an array that hold the color values.
@@ -11,8 +11,8 @@ Color.isColor = function(rgb){
   //each color must be between 0 and 255. Also can be equal.
 
   let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
-  let greenworks = (rgb[0] >= 0) && (rgb[0] <= 255);
-  let blueworks = (rgb[0] >= 0) && (rgb[0] <= 255);
+  let greenworks = (rbg[1] >= 0) && (rgb[1] <= 255)
+  let blueworks = (rgb[2] >=0) && (rgb[2] <= 255)
 
   if(redworks && greenworks && blueworks){
     return true;
@@ -23,16 +23,27 @@ Color.isColor = function(rgb){
 };
 
 Color.redIntensity = function(rgb){
-
+  Math.round((rgb[0]/255)*100)
+return redIntensity
 };
 
 //greenIntensity
-
+Color.greenIntensity = function(rgb){
+    Math.round((rgb[1]/255)*100)
+    return greenIntensity
+};
 
 //blueIntensity
-
+Color.blueIntensity = function(rgb){
+    Math.round((rgb[2]/255)*100);
+    return blueIntensity
+}
 
 //brightness
+function brightness(redIntensity,greenIntensity,blueIntensity){
+  const bright = (redIntensity+greenIntensity+blueIntensity)/3
+  return bright
 
+}
 
 //complement
